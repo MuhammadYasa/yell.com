90e281ec8053f42c038142759ee584c388651ba3
MYasa <nearhxh@gmail.com>
2022-03-06 23:05:38 +0700

    update scraping with all pagenation

--- main.py
+++ main.py
@@ -6,59 +6,100 @@ import pandas as pd
 import openpyxl
 
 
-
+base_url = 'https://www.yell.com/'
 url = 'https://www.yell.com/ucs/UcsSearchAction.do?'
 
-params = {
-    'keywords' : 'Italian Reataurants',
-    'location' : 'Houghton Le Spring',
-    'scrambleSeed' : '1581452655',
-}
 headers = {
     'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36',
 }
-base_url = 'https://www.yell.com/'
-result = []
 
-res = requests.get(url, params=params, headers=headers)
-soup = BeautifulSoup(res.text, 'html.parser')
-
-# scraping proccess
-headers_contents = soup.find_all('div', 'row businessCapsule--mainRow')
-
-for content in headers_contents:
-    title = content.find('h2', 'businessCapsule--name text-h2').text
-    classification = content.find('span', 'businessCapsule--classification').text
-    telephone = content.find('span', 'business--telephoneNumber')
-    if telephone is not None:
-        telephone = telephone.text
-    else:
-        telephone = 'Telephone is not available'
-    link_web = base_url + content.find('div', 'businessCapsule--titSpons').find('a')['href']
-
-    # final data
-    final_data = {
-        'title': title,
-        'classification': classification,
-        'telephone': telephone,
-        'link web': link_web,
+def get_total_pages(searches):
+    params = {
+        'keywords' : 'Italian Reataurants',
+        'location' : 'Houghton Le Spring',
+        'scrambleSeed' : '1581452655',
+        'key' : searches,
+        'pg' : 1,
     }
-    print(final_data)
-    result.append(final_data)
-print('jumlah datanya adalah', len(result))
 
-try:
-    os.mkdir('json_result')
-except FileExistsError:
-    pass
-with open('json_result/final_data.json', 'w+') as json_data:
-    json.dump(result, json_data)
-print('json created')
+    res = requests.get(url, params=params, headers=headers)
+    soup = BeautifulSoup(res.content, 'html.parser')
 
-# create csv
-df = pd.DataFrame(result)
-df.to_csv('yell_data.csv', index=False)
-df.to_excel('yell_data.xlsx', index=False)
+    pages = []
+    headers_contents = soup.find('div', 'col-sm-14 col-md-16 col-lg-14 text-center').find_all('a')
+    for i in headers_contents:
+        pages.append(int(i.text))
+
+    print(headers_contents)
+    total_pages = max(pages)
+    print(total_pages)
+    return total_pages
+
+def get_all_item(searches, pages):
+    restaurant_list = []
+    params = {
+        'keywords': 'Italian Reataurants',
+        'location': 'Houghton Le Spring',
+        'scrambleSeed': '1581452655',
+        'key': searches,
+        'pg': pages,
+    }
+    res = requests.get(url, params=params, headers=headers)
+
+    soup = BeautifulSoup(res.text, 'html.parser')
+    results = soup.find('div', {'class' : 'row results--row results--capsuleList'})
+    contents = results.find_all('div', {'class':'row businessCapsule--mainRow'})
+
+    for content in contents:
+        title = content.find('h2', 'businessCapsule--name text-h2').text
+        classification = content.find('span', 'businessCapsule--classification').text
+        link_web = base_url + content.find('div', 'businessCapsule--titSpons').find('a')['href']
+
+        telephone = content.find('span', 'business--telephoneNumber')
+        if telephone is not None:
+            telephone = telephone.text
+        else:
+            telephone = 'Telephone is not available'
+
+        final_data = {
+            'title': title,
+            'classification': classification,
+            'telephone': telephone,
+            'link web': link_web,
+        }
+
+        restaurant_list.append(final_data)
+
+    return restaurant_list
+
+def output(searches, final_result):
+    # export to csv or excel
+    df = pd.DataFrame(final_result)
+    df.to_csv(f'{searches}.csv', index=False)
+    df.to_excel(f'{searches}.xlsx', index=False)
+
+def main(searches):
+    final_result = []
+
+    total_pages = get_total_pages(searches)
+    for page in range (total_pages):
+        page += 1
+        print(f'Scraping halaman ke : {page}')
+        products = get_all_item(searches, page)
+        final_result += products
+
+    # the processing data
+    total_data = len(final_result)
+    print(f'total page after scraping {format(total_data)}')
+
+    # export to csv
+    output(searches, final_result)
+
+if __name__ == '__main__':
+    searches = ('restaurants')
+    main(searches)
+
+
 
 # data created
 print('Data Created Succes')
